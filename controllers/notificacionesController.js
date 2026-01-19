da1a10a85842207646d6bdfc5828c904937d1c16
Luis-avalos <Luis.avalos.luevano@gmail.com>
2026-01-19 12:57:59 -0600

    Add noticias web

--- controllers/notificacionesController.js
+++ controllers/notificacionesController.js
@@ -36,7 +36,6 @@ const crearNotificacion = async (req, res) => {
   }
 };
 
-
 const getNotificaciones = async (req, res) => {
   try {
     const result = await pool.query(`
@@ -52,7 +51,56 @@ const getNotificaciones = async (req, res) => {
   }
 };
 
+const crearNotificacionweb = async (req, res) => {
+  try {
+    const { titulo, mensaje } = req.body;
+
+    if (!titulo || !mensaje) {
+      return res.status(400).json({
+        error: 'titulo y mensaje son obligatorios'
+      });
+    }
+
+    const query = `
+      INSERT INTO vallemolinostest.notificacionesweb
+      (titulo, mensaje)
+      VALUES ($1, $2)
+      RETURNING *;
+    `;
+
+    const result = await pool.query(query, [titulo, mensaje]);
+    const notificacion = result.rows[0];
+
+    const io = req.app.get('io');
+    if (io) {
+      io.emit('nueva_notificacion', notificacion);
+    }
+
+    res.json(notificacion);
+  } catch (error) {
+    console.error('Error al crear notificación:', error);
+    res.status(500).json({ error: 'Error del servidor' });
+  }
+};
+
+const getNotificacionesweb = async (req, res) => {
+  try {
+    const result = await pool.query(`
+      SELECT *
+      FROM vallemolinostest.notificacionesweb
+      ORDER BY fecha DESC
+    `);
+
+    res.json(result.rows);
+  } catch (error) {
+    console.error('Error al listar notificaciones:', error);
+    res.status(500).json({ error: 'Error del servidor' });
+  }
+};
+
 module.exports = {
   crearNotificacion,
-  getNotificaciones
+  getNotificaciones,
+  getNotificacionesweb,
+  crearNotificacionweb
 };
--- routes/notificacionesRoutes.js
+++ routes/notificacionesRoutes.js
@@ -1,10 +1,11 @@
-
 const express = require('express');
-const { crearNotificacion,  getNotificaciones } = require('../controllers/notificacionesController');
+const { crearNotificacion,  getNotificaciones, crearNotificacionweb, getNotificacionesweb } = require('../controllers/notificacionesController');
 
 const router = express.Router();
 
 router.post('/', crearNotificacion);
 router.get('/', getNotificaciones);
 
+router.post('/web', crearNotificacionweb);
+router.get('/web', getNotificacionesweb);
 module.exports = router;
